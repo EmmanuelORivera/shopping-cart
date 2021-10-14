6e24ee92600cba8ed4aec59667c304dd35392d84
Emmanuel Ochoa <eorivera95@gmail.com>
2021-10-14 16:27:35 -0600

    changing the url from image with id 7, because it was broken

--- src/data.js
+++ src/data.js
@@ -67,7 +67,7 @@ export const popularProducts = [
   },
   {
     id: 7,
-    img: 'https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png',
+    img: 'https://assets.webiconspng.com/uploads/2017/09/Jacket-PNG-Image-62941.png',
   },
   {
     id: 8,
